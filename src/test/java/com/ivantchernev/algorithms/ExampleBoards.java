ff2172bfdeb9f7925ad54006df60ce0c62c428ca
Ivan Tchernev <itchernev@and.digital>
2018-09-16 17:37:41 +0100

    Added simple tests

--- src/test/java/com/ivantchernev/algorithms/ExampleBoards.java
+++ src/test/java/com/ivantchernev/algorithms/ExampleBoards.java
@@ -35,6 +35,12 @@ public class ExampleBoards {
             {0, 5, 1}
     };
 
+    static final int[][] threeBoardUnsolvable = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {8, 7, 0}
+    };
+
     static final int[][] fourBoardGoal = {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
@@ -47,5 +53,23 @@ public class ExampleBoards {
             {9, 2, 7, 8},
             {13, 0, 10, 12},
             {14, 6, 11, 15}
+    };
+
+    static final int[][] getThreeBoardPuzzleFourStepOne = {
+            {1, 0, 3},
+            {4, 2, 5},
+            {7, 8, 6}
+    };
+
+    static final int[][] getThreeBoardPuzzleFourStepTwo = {
+            {1, 2, 3},
+            {4, 0, 5},
+            {7, 8, 6}
+    };
+
+    static final int[][] getThreeBoardPuzzleFourStepThree = {
+            {1, 2, 3},
+            {4, 5, 0},
+            {7, 8, 6}
     };
 }
--- src/test/java/com/ivantchernev/algorithms/SolverTest.java
+++ src/test/java/com/ivantchernev/algorithms/SolverTest.java
@@ -2,14 +2,84 @@ package com.ivantchernev.algorithms;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolverTest {
 
     @Test
-    void SolverThrowsIllegalArgumentExceptionIfPassedNullBoard() {
+    void solverThrowsIllegalArgumentExceptionIfPassedNullBoard() {
         assertThrows(IllegalArgumentException.class,
                 () -> new Solver(null));
     }
 
+    @Test
+    void solverSolvesTwoGoalBoard() {
+        Board twoGoalBoard = new Board(ExampleBoards.twoBoardGoal);
+        Solver s = new Solver(twoGoalBoard);
+
+        assertTrue(s.isSolvable());
+        assertEquals(0, s.moves());
+        assertEquals(twoGoalBoard, ((Stack<Board>) s.solution()).pop());
+    }
+
+    @Test
+    void solverSolvesThreeGoalBoard() {
+        Board threeGoalBoard = new Board(ExampleBoards.threeBoardGoal);
+        Solver s = new Solver(threeGoalBoard);
+
+        assertTrue(s.isSolvable());
+        assertEquals(0, s.moves());
+        assertEquals(threeGoalBoard, ((Stack<Board>) s.solution()).pop());
+    }
+
+    @Test
+    void solverSolvesFourGoalBoard() {
+        Board fourGoalBoard = new Board(ExampleBoards.fourBoardGoal);
+        Solver s = new Solver(fourGoalBoard);
+
+        assertTrue(s.isSolvable());
+        assertEquals(0, s.moves());
+        assertEquals(fourGoalBoard, ((Stack<Board>) s.solution()).pop());
+    }
+
+    @Test
+    void solverSolvesThreeBoardOneAway() {
+        Board initialBoard = new Board(ExampleBoards.threeBoardOneAway);
+        Solver s = new Solver(initialBoard);
+
+        assertTrue(s.isSolvable());
+        assertEquals(1, s.moves());
+
+        Stack<Board> solutionBoards = (Stack<Board>) s.solution();
+        assertEquals(initialBoard, solutionBoards.pop());
+        assertEquals(new Board(ExampleBoards.threeBoardGoal), solutionBoards.pop());
+    }
+
+    @Test
+    void solverSolvesThreeBoardPuzzleFour() {
+        Board initialBoard = new Board(ExampleBoards.threeBoardPuzzleFour);
+        Solver s = new Solver(initialBoard);
+
+        assertTrue(s.isSolvable());
+        assertEquals(4, s.moves());
+
+        Stack<Board> solutionBoards = (Stack<Board>) s.solution();
+        assertEquals(initialBoard, solutionBoards.pop());
+        assertEquals(new Board(ExampleBoards.getThreeBoardPuzzleFourStepOne), solutionBoards.pop());
+        assertEquals(new Board(ExampleBoards.getThreeBoardPuzzleFourStepTwo), solutionBoards.pop());
+        assertEquals(new Board(ExampleBoards.getThreeBoardPuzzleFourStepThree), solutionBoards.pop());
+        assertEquals(new Board(ExampleBoards.threeBoardGoal), solutionBoards.pop());
+    }
+
+    @Test
+    void solverFailsToSolveUnsolvableThreeByThree() {
+        Board initialBoard = new Board(ExampleBoards.threeBoardUnsolvable);
+        Solver s = new Solver(initialBoard);
+
+        assertFalse(s.isSolvable());
+        assertEquals(-1, s.moves());
+        assertEquals(null, s.solution());
+    }
 }
